301916c9cc43e7c82c172a48e77b4f62a875390b
Chonwai Un <chonwaiun@outlook.com>
2020-10-21 12:15:04 +0800

    Add: Added the help page.

---
+++ pages/help.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="help-page-index">
+        <div class="p-8">
+            <section id="Dataset" class="mb-8">
+                <h2 class="text-4xl">Help</h2>
+                <h3 class="text-2xl">Input – Sequences</h3>
+                <p>
+                    Datasets are provided here for the purpose of reproducibility and future method
+                    testing. If you are looking for information about AMP sequences, you are
+                    recommended to visit other AMP databases such as ADP, CAMP, LAMP, etc. Our
+                    datasets here were collected from these databases and filtered out sequences
+                    with non-natural amino acids.
+                </p>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelpPageIndex',
+};
+</script>
+
+<style lang="scss" scoped></style>
