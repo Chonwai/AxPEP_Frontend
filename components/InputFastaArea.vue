3ad3f7fd63be96a815a5b554e61a4f285523fed9
Chonwai <chonwaiun@gmail.com>
2020-12-01 19:25:04 +0800

    Add: Added the InputFastaArea Component.

---
+++ components/InputFastaArea.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="input-fasta-area-container">
+        <v-textarea
+            v-show="source === 'textarea'"
+            outlined
+            label="Input Sequences"
+            value=""
+        ></v-textarea>
+        <v-file-input
+            v-show="source === 'upload'"
+            show-size
+            v-model="files"
+            accept=".fasta"
+            placeholder="Upload your FASTA file"
+            label="File input"
+            prepend-icon="mdi-paperclip"
+        >
+            <template v-slot:selection="{ text }">
+                <v-chip small label color="primary">
+                    {{ text }}
+                </v-chip>
+            </template>
+        </v-file-input>
+        <div>
+            <v-radio-group v-model="source" row>
+                <v-radio label="Type manually" value="textarea"></v-radio>
+                <v-radio label="Upload FASTA sequences (.fasta)" value="upload"></v-radio>
+                <v-radio label="Option 2" value="radio-2"></v-radio>
+            </v-radio-group>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputFASTAArea',
+    data() {
+        return {
+            source: 'textarea',
+            files: [],
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,85 +1,77 @@
 <template>
     <v-row justify="center" align="center">
-        <v-col cols="12" sm="8" md="6">
-            <div class="text-center">
-                <logo />
-                <vuetify-logo />
-            </div>
-            <v-card>
-                <v-card-title class="headline">
-                    Welcome to the Vuetify + Nuxt.js template
-                </v-card-title>
-                <v-card-text>
-                    <p>
-                        Vuetify is a progressive Material Design component framework for Vue.js. It
-                        was designed to empower developers to create amazing applications.
-                    </p>
-                    <p>
-                        For more information on Vuetify, check out the
-                        <a href="https://vuetifyjs.com" target="_blank" rel="noopener noreferrer">
-                            documentation </a
-                        >.
-                    </p>
-                    <p>
-                        If you have questions, please join the official
-                        <a
-                            href="https://chat.vuetifyjs.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            title="chat"
-                        >
-                            discord </a
-                        >.
-                    </p>
-                    <p>
-                        Find a bug? Report it on the github
-                        <a
-                            href="https://github.com/vuetifyjs/vuetify/issues"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            title="contribute"
-                        >
-                            issue board </a
-                        >.
-                    </p>
-                    <p>
-                        Thank you for developing with Vuetify and I look forward to bringing more
-                        exciting features in the future.
-                    </p>
-                    <div class="text-xs-right">
-                        <em><small>&mdash; John Leider</small></em>
-                    </div>
-                    <hr class="my-3" />
-                    <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">
-                        Nuxt Documentation
-                    </a>
-                    <br />
-                    <a
-                        href="https://github.com/nuxt/nuxt.js"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Nuxt GitHub
-                    </a>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer />
-                    <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-                </v-card-actions>
-            </v-card>
+        <v-col cols="12" sm="8" md="12">
+            <v-stepper class="stepper-container" v-model="e6" vertical>
+                <v-stepper-step :complete="e6 > 1" step="1">
+                    Input sequences in FASTA format (length >= 5)
+                </v-stepper-step>
+
+                <v-stepper-content step="1">
+                    <InputFastaArea class="py-2" />
+                    <v-btn color="primary" @click="e6 = 2"> Continue </v-btn>
+                    <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step :complete="e6 > 2" step="2"> Prediction Methods </v-stepper-step>
+
+                <v-stepper-content step="2">
+                    <v-checkbox
+                        v-model="checkbox"
+                        label="Anti-microbial peptide (AmPEP)"
+                    ></v-checkbox>
+                    <v-checkbox
+                        v-model="checkbox"
+                        label="Short anti-microbial peptide (Deep-AmPEP30, for length <=30)"
+                    ></v-checkbox>
+                    <v-checkbox
+                        v-model="checkbox"
+                        label="Short anti-microbial peptide (RF-AmPEP30, for length <=30)"
+                    ></v-checkbox>
+                    <v-btn color="primary" @click="e6 = 3"> Continue </v-btn>
+                    <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step :complete="e6 > 3" step="3">
+                    Job Description (optional)
+                </v-stepper-step>
+
+                <v-stepper-content step="3">
+                    <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+                    <v-btn color="primary" @click="e6 = 4"> Continue </v-btn>
+                    <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step step="4">
+                    Create account to retrieve all submitted jobs</v-stepper-step
+                >
+                <v-stepper-content step="4">
+                    <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+                    <v-btn color="primary" @click="e6 = 1"> Continue </v-btn>
+                    <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+            </v-stepper>
         </v-col>
     </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-import VuetifyLogo from '~/components/VuetifyLogo.vue';
+import InputFastaArea from '../components/InputFastaArea';
 
 export default {
+    data() {
+        return {
+            e6: 1,
+        };
+    },
     name: 'HomePageIndex',
     components: {
-        Logo,
-        VuetifyLogo,
+        InputFastaArea,
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.stepper-container {
+    box-shadow: none !important;
+}
+</style>
