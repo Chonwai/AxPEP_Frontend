bdb5512d3e9d2aab08787aded9a918065cef7bca
Chonwai <chonwaiun@gmail.com>
2020-12-14 00:20:14 +0800

    Add: Added the newTaskByTextarea API.

--- apis/task.js
+++ apis/task.js
@@ -5,11 +5,19 @@ function newTaskByFile(param, data) {
     );
 }
 
+function newTaskByTextarea(param, data) {
+    return window.$nuxt.$api.$post(
+        `axpep/tasks/textarea?ampep=${param.ampep}&deepampep30=${param.deepampep30}&rfampep30=${param.rfampep30}`,
+        data
+    );
+}
+
 function getSpecifyTasksByEmail(data, page = 1) {
     return window.$nuxt.$api.$get(`axpep/emails/${data}/tasks?page=${page}`);
 }
 
 export default {
     newTaskByFile,
+    newTaskByTextarea,
     getSpecifyTasksByEmail,
 };
