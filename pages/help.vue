3ef262ade4ab7c473b2f082fd98af4289e7a82e7
Chonwai <chonwaiun@gmail.com>
2021-01-25 01:45:05 +0800

    Update: Updated the help page.

--- pages/help.vue
+++ pages/help.vue
@@ -76,6 +76,28 @@
                 </p>
             </div>
         </section>
+        <section id="Contact" class="mb-8">
+            <h2 class="text-4xl">Contact</h2>
+            <p>If you have further questions, please contact:</p>
+            <h3 class="text-2xl">Developer:</h3>
+            <ul>
+                <li>
+                    <span class="font-bold">Pratiti Bhadra</span>
+                    <code>pratiti.bhadra@gmail.com</code>
+                </li>
+                <li><span class="font-bold">Jielu Yan</span> <code>oyanjielu@gmail.com</code></li>
+                <li>
+                    <span class="font-bold">Edison Un (web developer)</span>
+                    <code>chonwaiun@gmail.com</code>
+                </li>
+            </ul>
+            <h3 class="text-2xl">PI:</h3>
+            <ul>
+                <li>
+                    <span class="font-bold">Shirley W. I. Siu</span> <code>shirleysiu@umac.mo</code>
+                </li>
+            </ul>
+        </section>
     </div>
 </template>
 
