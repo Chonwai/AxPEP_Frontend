a821cf97d0351e5eb749fbbc2aa3d0723e049abb
Chonwai <chonwaiun@gmail.com>
2021-02-26 20:20:53 +0800

    Update: Updated the nuxt.config.js.

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,19 +76,4 @@ export default {
 
     // Build Configuration (https://go.nuxtjs.dev/config-build)
     build: {},
-
-    generate: {
-        routes(callback) {
-            axios
-                .get('http://159.203.14.63/retrieve')
-                .then(res => {
-                    const routes = res.data.map(retrieve => {
-                        console.log(retrieve);
-                        return '/retrieve/' + retrieve.email;
-                    });
-                    callback(null, routes);
-                })
-                .catch(callback);
-        },
-    },
 };
