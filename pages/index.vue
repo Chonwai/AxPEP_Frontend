ae984ff0a6732b72048e3c7a664630110ac6b9b0
Chonwai <chonwaiun@gmail.com>
2021-03-17 16:40:32 +0800

    Add: Added the landing page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,58 @@
 <template>
     <div>
-        <v-parallax dark src="images/banner.jpg">
-            <v-row align="center" justify="center">
-                <v-col class="text-center" cols="12">
-                    <h1 class="text-4xl font-normal mb-4">
-                        Computational Biology and Bioinformatics Lab (CBBio)
-                    </h1>
-                </v-col>
-            </v-row>
-        </v-parallax>
-        <section>
-            <v-container>
-                <h2 class="text-2xl font-normal mb-4">Software</h2>
-            </v-container>
-        </section>
+        <div
+            class="flex flex-col items-center justify-center h-60vh mb-4 bg-fixed bg-center bg-cover banner-img"
+        >
+            <h1 class="text-4xl text-white font-normal">
+                Computational Biology and Bioinformatics Lab (CBBio)
+            </h1>
+            <h4 class="text-base text-white font-normal">
+                Department of Computer and Information Science University of Macau
+            </h4>
+        </div>
+        <v-container class="min-h-80vh">
+            <section class="py-4">
+                <h2 class="text-2xl font-medium mb-4">Software</h2>
+                <p>We share Bioinformatics software and data to facilitate research.</p>
+                <v-chip-group mandatory active-class="primary--text mb-4">
+                    <v-chip>AmPEP</v-chip>
+                    <v-chip>AcPEP</v-chip>
+                    <v-chip>BESTox</v-chip>
+                    <v-chip>LigTMap</v-chip>
+                    <v-chip>PSOVina</v-chip>
+                    <v-chip>GWOVina</v-chip>
+                    <v-chip>TMDIM</v-chip>
+                    <v-chip>ProtPOS</v-chip>
+                    <v-chip>PyMOL mControl</v-chip>
+                </v-chip-group>
+            </section>
+            <v-divider></v-divider>
+            <section class="py-4">
+                <h2 class="text-2xl font-medium mb-4">Applications</h2>
+                <div class="my-4" v-for="(item, i) in cardItems" :key="i">
+                    <v-card :color="item.color" dark>
+                        <div class="d-flex flex-no-wrap justify-space-between">
+                            <div>
+                                <v-card-title v-text="item.title" class="headline"
+                                    >AmPEP | AxPEP</v-card-title
+                                >
+                                <v-card-subtitle v-text="item.subtitle"></v-card-subtitle>
+                                <v-card-actions>
+                                    <v-btn class="ml-2 mt-5" outlined rounded small> START </v-btn>
+                                </v-card-actions>
+                            </div>
+                            <v-avatar class="ma-3 h-full" width="33.3333%" height="100%" tile>
+                                <v-img
+                                    position="center center"
+                                    :contain="true"
+                                    :src="item.src"
+                                ></v-img>
+                            </v-avatar>
+                        </div>
+                    </v-card>
+                </div>
+            </section>
+        </v-container>
     </div>
 </template>
 
@@ -21,10 +60,36 @@
 export default {
     name: 'HomePageIndex',
     data() {
-        return {};
+        return {
+            cardItems: [
+                {
+                    color: '#3f51b5',
+                    src: '/images/AmPEPCard.png',
+                    title: 'AmPEP | AxPEP',
+                    subtitle: 'Sequence-based classification of Antimicrobial peptides',
+                },
+                {
+                    color: '#1C2833',
+                    src: '/images/BESToxCard.png',
+                    title: 'BESTox',
+                    subtitle:
+                        'Convolutional neural network regression model for acute oral toxicity prediction',
+                },
+                {
+                    color: '#89cff0',
+                    src: '/images/LigTMapCard.png',
+                    title: 'LigTMap',
+                    subtitle: 'Target and activity prediction server for small molecules',
+                },
+            ],
+        };
     },
     methods: {},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner-img {
+    background-image: url('/images/banner.jpg');
+}
+</style>
