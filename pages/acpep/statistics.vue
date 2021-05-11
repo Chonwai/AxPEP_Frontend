72ff3f17af696234f0d710499ef645d4a0e5b1b8
Chonwai <chonwaiun@gmail.com>
2021-05-11 18:10:08 +0800

    Refactor: Refactored the StatisticsPage.

--- pages/acpep/statistics.vue
+++ pages/acpep/statistics.vue
@@ -1,127 +1,24 @@
 <template>
-    <div id="statistics-page-index" class="p-0 md:p-8">
-        <section id="Statistics" class="mb-8">
-            <h2 class="text-4xl">
-                Statistics <span class="text-lg">({{ daysAgo }} days)</span>
-            </h2>
-            <div class="flex flex-row items-center py-2">
-                <p class="mb-0">Select the Period:</p>
-                <v-btn-toggle v-model="daysAgo" tile color="success" group>
-                    <v-btn :value="7"> 7 Days </v-btn>
-                    <v-btn :value="14"> 14 Days </v-btn>
-                    <v-btn :value="30"> 30 Days </v-btn>
-                    <v-btn :value="180"> 180 Days </v-btn>
-                    <v-btn :value="365"> 365 Days </v-btn>
-                </v-btn-toggle>
-            </div>
-            <div id="map-wrap z-10" class="h-80 mb-8">
-                <l-map :zoom="1" :center="[25, 0]">
-                    <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-                    <div v-if="locationsList != []">
-                        <l-marker
-                            v-for="(location, index) in locationsList"
-                            :key="index"
-                            :lat-lng="[location.latitude, location.longitude]"
-                        ></l-marker>
-                    </div>
-                </l-map>
-            </div>
-            <div class="line-chart mb-8">
-                <p>Total {{ totalJobs }} jobs on period {{ daysAgo }} days</p>
-                <LineChart
-                    :height="360"
-                    :labels="labels"
-                    :data="chartData"
-                    :dataName="chartDataName"
-                    :color="chartColor"
-                />
-            </div>
-            <div class="doughnut-chart mb-8">
-                <p>How often are methods used?</p>
-                <DoughnutChart
-                    :height="360"
-                    :labels="doughnutLabels"
-                    :data="doughnutChartData"
-                    :dataName="doughnutChartDataName"
-                    :color="doughnutChartColor"
-                />
-            </div>
-        </section>
-    </div>
+    <StatisticsPage
+        application="acpep"
+        :lineChartColor="lineChartColor"
+        :doughnutChartColor="doughnutChartColor"
+    ></StatisticsPage>
 </template>
 
 <script>
-import LineChart from '@/components/LineChart';
-import DoughnutChart from '@/components/DoughnutChart';
-import AnalysisAPI from '@/apis/analysis';
+import StatisticsPage from '@/components/StatisticsPage';
 export default {
     layout: 'acpep',
-    name: 'StatisticsPageIndex',
+    name: 'AcPEPStatisticsPageIndex',
     data() {
         return {
-            daysAgo: 14,
-            locationsList: [],
-            labels: [],
-            doughnutLabels: [],
-            chartData: [],
-            doughnutChartData: [],
-            chartDataName: 'Jobs',
-            doughnutChartDataName: 'Each Methods',
-            totalJobs: 0,
-            chartColor: '#c0d9c2',
+            lineChartColor: '#c0d9c2',
             doughnutChartColor: ['#ED6D0D', '#F2A805', '#FEE4B5', '#9BD185', '#7CC26A', '#649748'],
         };
     },
     components: {
-        LineChart,
-        DoughnutChart,
-    },
-    async created() {
-        await this.init();
-    },
-    watch: {
-        daysAgo() {
-            this.init();
-        },
-    },
-    methods: {
-        async init() {
-            this.getCountLocations();
-            this.getCountTasks();
-            this.getCountEachMethods();
-        },
-        async getCountLocations() {
-            let res = await AnalysisAPI.getCountingLocationsByNDaysAgo(this.daysAgo);
-            this.locationsList = res.message;
-        },
-        async getCountTasks() {
-            let res = await AnalysisAPI.getCountingTasksByNDaysAgo(this.daysAgo);
-            let dateList = [];
-            let countList = [];
-            let totalCount = 0;
-            for (const item of res.message) {
-                dateList.push(item.date);
-                countList.push(item.total);
-                totalCount += item.total;
-            }
-            this.labels = dateList;
-            this.chartData = countList;
-            this.totalJobs = totalCount;
-        },
-        async getCountEachMethods() {
-            let res = await AnalysisAPI.getCountingEachMethods('acpep');
-            console.log(res);
-            let dateList = [];
-            let countList = [];
-            let totalCount = 0;
-            for (const item of res.message) {
-                dateList.push(item.method);
-                countList.push(item.total);
-                totalCount += item.total;
-            }
-            this.doughnutLabels = dateList;
-            this.doughnutChartData = countList;
-        },
+        StatisticsPage,
     },
 };
 </script>
--- pages/ampep/statistics.vue
+++ pages/ampep/statistics.vue
@@ -1,94 +1,24 @@
 <template>
-    <div id="statistics-page-index" class="p-0 md:p-8">
-        <section id="Statistics" class="mb-8">
-            <h2 class="text-4xl">
-                Statistics <span class="text-lg">({{ daysAgo }} days)</span>
-            </h2>
-            <div class="flex flex-row items-center py-2">
-                <p class="mb-0">Select the Period:</p>
-                <v-btn-toggle v-model="daysAgo" tile color="deep-purple accent-3" group>
-                    <v-btn :value="7"> 7 Days </v-btn>
-                    <v-btn :value="14"> 14 Days </v-btn>
-                    <v-btn :value="30"> 30 Days </v-btn>
-                    <v-btn :value="180"> 180 Days </v-btn>
-                    <v-btn :value="365"> 365 Days </v-btn>
-                </v-btn-toggle>
-            </div>
-            <div id="map-wrap z-10" class="h-80 mb-8">
-                <l-map :zoom="1" :center="[25, 0]">
-                    <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-                    <div v-if="locationsList != []">
-                        <l-marker
-                            v-for="(location, index) in locationsList"
-                            :key="index"
-                            :lat-lng="[location.latitude, location.longitude]"
-                        ></l-marker>
-                    </div>
-                </l-map>
-            </div>
-            <div class="line-chart mb-8">
-                <p>Total {{ totalJobs }} jobs on period {{ daysAgo }} days</p>
-                <LineChart
-                    :height="360"
-                    :labels="labels"
-                    :data="chartData"
-                    :dataName="chartDataName"
-                />
-            </div>
-        </section>
-    </div>
+    <StatisticsPage
+        application="ampep"
+        :lineChartColor="lineChartColor"
+        :doughnutChartColor="doughnutChartColor"
+    ></StatisticsPage>
 </template>
 
 <script>
-import LineChart from '@/components/LineChart';
-import AnalysisAPI from '@/apis/analysis';
+import StatisticsPage from '@/components/StatisticsPage';
 export default {
     layout: 'ampep',
-    name: 'StatisticsPageIndex',
+    name: 'AmPEPStatisticsPageIndex',
     data() {
         return {
-            daysAgo: 14,
-            locationsList: [],
-            labels: [],
-            chartData: [],
-            chartDataName: 'Jobs',
-            totalJobs: 0,
+            lineChartColor: '#3f51b5',
+            doughnutChartColor: ['#3f51b5', '#69f0ae', '#0288d1', '#00bfa5'],
         };
     },
     components: {
-        LineChart,
-    },
-    async created() {
-        await this.init();
-    },
-    watch: {
-        daysAgo() {
-            this.init();
-        },
-    },
-    methods: {
-        async init() {
-            this.getCountLocations();
-            this.getCountTasks();
-        },
-        async getCountLocations() {
-            let res = await AnalysisAPI.getCountingLocationsByNDaysAgo(this.daysAgo);
-            this.locationsList = res.message;
-        },
-        async getCountTasks() {
-            let res = await AnalysisAPI.getCountingTasksByNDaysAgo(this.daysAgo);
-            let dateList = [];
-            let countList = [];
-            let totalCount = 0;
-            for (const item of res.message) {
-                dateList.push(item.date);
-                countList.push(item.total);
-                totalCount += item.total;
-            }
-            this.labels = dateList;
-            this.chartData = countList;
-            this.totalJobs = totalCount;
-        },
+        StatisticsPage,
     },
 };
 </script>
