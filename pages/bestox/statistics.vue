30b388e12817d048ec007f44465013ffcf1b6ef2
Chonwai <chonwaiun@gmail.com>
2021-06-08 13:55:47 +0800

    Add: Added the statistics page on BESTox.

---
+++ pages/bestox/statistics.vue
@@ -0,0 +1,24 @@
+<template>
+    <StatisticsPage
+        application="bestox"
+        :lineChartColor="lineChartColor"
+        :doughnutChartColor="doughnutChartColor"
+    ></StatisticsPage>
+</template>
+
+<script>
+import StatisticsPage from '@/components/StatisticsPage';
+export default {
+    layout: 'bestox',
+    name: 'AcPEPStatisticsPageIndex',
+    data() {
+        return {
+            lineChartColor: '#c0d9c2',
+            doughnutChartColor: ['#ED6D0D', '#F2A805', '#FEE4B5', '#9BD185', '#7CC26A', '#649748'],
+        };
+    },
+    components: {
+        StatisticsPage,
+    },
+};
+</script>
